d370fcd0e3816d2434e9c3a0af704f566e8f31f0
hermannmanuel <hermann.manuel>
2024-03-25 11:48:32 +0100

    Ajout du bouton "more info"

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -2,6 +2,7 @@ import { IconDefinition } from "@fortawesome/free-brands-svg-icons";
 import { faCircleInfo, faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[] = [
-  //faCircleInfo
-  faPlay
+
+  faPlay,
+  faCircleInfo
 ]
